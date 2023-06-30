e137ef2ace86d2392fd247a837c3b98de499d046
Antonio Sérgio <antoniossborges@gmail.com>
2023-06-29 21:07:34 -0300

    #

--- cadastroPessoas/src/Main.java
+++ cadastroPessoas/src/Main.java
@@ -15,24 +15,46 @@ public class Main {
         List<Person> studants = new ArrayList<Person>();
         boolean exit = true;
         while (exit){
-            Person studant = new Person();
             Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Digite o nome completo do estudante");
-            studant.name = scanner.nextLine();
-            studants.add(studant);
-            System.out.println("Obrigado por digitar o nome completo do " + studant.name);
-
-
-            System.out.println("Deseja cadastrar mais um? S/SIM e N/NÃO");
-            String option = scanner.next();
-            if(option.equals("N")){
-                exit = false;
+            System.out.println("Seleciona a opção");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Buscar");
+            System.out.println("3 - Sair");
+            int option = scanner.nextInt();
+            switch (option){
+                case 1 :
+                    addStudant(studants);
+                    break;
+                case 2 :
+                    System.out.println("Qual o nome que deseja buscar?");
+                    String nameToFind = scanner.nextLine();
+                    findStudant(studants, nameToFind);
+                    break;
+                case 3 :
+                    exit = false;
+                    break;
             }
-
         }
+    }
 
+    private static void addStudant(List<Person> studants){
+        Person studant = new Person();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o nome completo do estudante");
+        studant.name = scanner.nextLine();
+        studants.add(studant);
+        System.out.println("Obrigado por digitar o nome completo do " + studant.name);
+    }
 
+    private static void findStudant(List<Person> studants, String studantName) {
+
+        for(int count = 0; count < studants.size(); count++){
+            Person student = studants.get(count);
+            if(student.equals(studantName)){
+                System.out.println("Encontrei o " + studantName);
+            }
+        }
     }
 }
