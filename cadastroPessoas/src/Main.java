41864e394156bc3be1fbdd792cf0acd7dfae705e
Antonio Sérgio <antoniossborges@gmail.com>
2023-07-06 19:20:54 -0300

    #Adicionado método remover e editar

--- cadastroPessoas/src/Main.java
+++ cadastroPessoas/src/Main.java
@@ -9,17 +9,18 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static List<Person> studants = new ArrayList<Person>();
     public static void main(String[] args) {
 
-        List<Person> studants = new ArrayList<Person>();
         boolean exit = true;
         while (exit){
-            Scanner scanner = new Scanner(System.in);
-
             System.out.println("Seleciona a opção");
             System.out.println("1 - Cadastrar");
             System.out.println("2 - Buscar");
-            System.out.println("3 - Sair");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Editar");
+            System.out.println("5 - Sair");
             int option = scanner.nextInt();
             switch (option){
                 case 1 :
@@ -31,11 +32,42 @@ public class Main {
                     findStudant(studants, nameToFind);
                     break;
                 case 3 :
+                    removeStudant();
+                    break;
+                case 4 :
+                    editStudant();
+                    break;
+
+
+                case 5 :
                     exit = false;
                     break;
             }
         }
     }
+
+    private static void removeStudant(){
+        System.out.println("Qual o nome que deseja remover?");
+        String name = scanner.next();
+        Integer position = findStudant(studants, name);
+        if(position != null){
+            studants.remove(position.intValue());
+        }
+    }
+
+    private static void editStudant(){
+        System.out.println("Qual o nome que deseja editar?");
+        String name = scanner.next();
+        Integer position = findStudant(studants, name);
+        if(position != null){
+            Person p1 = studants.get(position.intValue());
+            System.out.println("Qual é o novo nome?");
+            name = scanner.next();
+            //studants.get(position.intValue()).name = name;
+            p1.name = name;
+        }
+    }
+
 
     private static void addStudant(List<Person> studants){
         Person studant = new Person();
@@ -47,13 +79,15 @@ public class Main {
         System.out.println("Obrigado por digitar o nome completo do " + studant.name);
     }
 
-    private static void findStudant(List<Person> studants, String studantName) {
-
+    private static Integer findStudant(List<Person> studants, String studantName) {
+        Integer position = null;
         for(int count = 0; count < studants.size(); count++){
             Person student = studants.get(count);
             if(student.name.equals(studantName)){
+                position = count;
                 System.out.println("Encontrei o " + studantName);
             }
         }
+        return position;
     }
 }
